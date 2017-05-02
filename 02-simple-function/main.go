4f4a0550ae09ebdd3bb746669aeab2d717150938
timostrating <timostrating@home.nl>
2017-05-02 23:58:19 +0200

    added workshops files

---
+++ 02-simple-function/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("4 + 5 =", add(4, 5))
+}
+
+func add(a int, b int) int {
+	return a + b;
+}
+
+// Implement the function add which takes a and b, and returns the sum
---
+++ 14-defer/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// write a defer statement that prints "Goodbye" on this line (remember, defer needs a function invocation!)
+	fmt.Print("Hello ")
+}
