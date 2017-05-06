7750700a6865dd1c41bf5cbda6d9f0909de36196
Timo Strating <timostrating@users.noreply.github.com>
2017-05-06 23:40:29 +0200

    solved nr 4

--- 04-returning-functions/main.go
+++ 04-returning-functions/main.go
@@ -4,8 +4,15 @@ import "fmt"
 
 func main() {
 	you := yourName("Gopher, because I write GO!")
-	fmt.Println("Hello", you())
+	fmt.Println("Hello", you)
 }
 
+func yourName(str string) string {
+  return yourName2(str)
+}
+
+func yourName2(str string) string {
+  return str
+}
 // Implement a function yourName that takes a string
 // return a function that returns a string when it's invoked
